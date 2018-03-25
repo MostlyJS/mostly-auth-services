cb5415dcd332167352e648e884d2b0eac51b99c4
forafish <fora.luo@gmail.com>
2018-03-25 19:54:59 +0800

    entities/models filename

--- src/entities/index.js
+++ src/entities/index.js
@@ -2,8 +2,8 @@ import glob from 'glob';
 import path from 'path';
 
 // load all entities
-let entityFiles = glob.sync(path.join(__dirname, './*-entity.js'));
+let entityFiles = glob.sync(path.join(__dirname, './*.entity.js'));
 entityFiles.forEach(file => {
-  let name = path.basename(file, '-entity.js');
+  let name = path.basename(file, '.entity.js');
   module.exports[name] = require(file);
 });
--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,8 @@ import glob from 'glob';
 import path from 'path';
 
 // load all models
-let modelFiles = glob.sync(path.join(__dirname, './*-model.js'));
+let modelFiles = glob.sync(path.join(__dirname, './*.model.js'));
 modelFiles.forEach(file => {
-  let name = path.basename(file, '-model.js');
+  let name = path.basename(file, '.model.js');
   module.exports[name] = require(file);
 });
