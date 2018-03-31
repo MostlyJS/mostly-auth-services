83678f870b798a88dcbc1ce53a2d606bda406065
forafish <fora.luo@gmail.com>
2018-03-31 23:36:09 +0800

    const variable

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import path from 'path';
 // load all services
 const serviceFiles = glob.sync(path.join(__dirname, './services/*/*.service.js'));
 export default Object.assign({}, ...serviceFiles.map(file => {
-  let name = camelCase(path.basename(path.dirname(file)));
+  const name = camelCase(path.basename(path.dirname(file)));
   return { [name]: require(file).default };
 }));
