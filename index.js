c731645214ec81ac26af0cbfd7afdddddefc322a
forafish <fora.luo@gmail.com>
2018-04-02 03:10:06 +0800

    add time log for esm loader

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require = require("esm")(module/*, options*/);
+console.time('mostly-auth-services import');
 module.exports = require('./src/index').default;
 module.exports.entities = require('./src/entities').default;
 module.exports.models = require('./src/models').default;
+console.timeEnd('mostly-auth-services import');
